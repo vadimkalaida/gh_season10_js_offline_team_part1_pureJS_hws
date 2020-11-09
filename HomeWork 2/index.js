b24c5e2b96183108c0fd13b1d46ed4f53b0d7421
vadimkalayda <vadosgta1@gmail.com>
2020-11-09 12:32:55 +0200

    findSum() and findMax() are ready

--- HomeWork 2/index.js
+++ HomeWork 2/index.js
@@ -1,4 +1,31 @@
+Object.prototype.filterArrayNumber = function() {
+  let filteredArray = [];
+
+  for(let i = 0; i < this.length; i++) {
+    if(!isNaN(this[i] * 1)) {
+      
+      filteredArray.push(this[i] * 1);
+    } else {
+      console.log(`${this[i]} - is not a number`);
+    }
+  }
+
+  return filteredArray;
+};
+
 Object.prototype.findMax = function () {
+  let myArray = this.filterArrayNumber();
+  let myMax = myArray[0];
+
+  for(let i = 0; i < myArray.length; i++) {
+    if(myArray[i] > myMax) {
+      myMax = myArray[i];
+    } else {
+      console.log(`${myArray[i]} is not more than ${myMax}`);
+    }
+  }
+
+  return myMax;
 
 };
 
@@ -8,9 +35,10 @@ Object.prototype.findMin = function () {
 
 Object.prototype.findSum = function () {
   let mySum = 0;
+  let myArray = this.filterArrayNumber();
 
-  for(let i = 0; i < this.length; i++) {
-    mySum += this[i];
+  for(let i = 0; i < myArray.length; i++) {
+    mySum += myArray[i];
   }
 
   return mySum;
