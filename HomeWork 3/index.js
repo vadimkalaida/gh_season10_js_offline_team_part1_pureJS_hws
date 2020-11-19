623ed46ab8a833fc688e115043bb3e4713920e40
vadimkalayda <vadosgta1@gmail.com>
2020-11-19 23:44:12 +0200

    task 5 is almost ready

--- HomeWork 3/index.js
+++ HomeWork 3/index.js
@@ -35,7 +35,62 @@ const sum = (a) => {
   return curry;
 };
 
-console.log('TASK 2: sum(num1)(num2)...');
-console.log(`TASK 2: sum(2)(4)(6) = ${sum(2)(4)(6)}`);
-console.log(`TASK 2: sum(23)(4) = ${sum(23)(4)}`);
-console.log(`TASK 2: sum(2)(4)(4)(7)(4) = ${sum(2)(4)(4)(7)(4)}`);
+console.log('TASK 3: sum(num1)(num2)...');
+console.log(`TASK 3: sum(2)(4)(6) = ${sum(2)(4)(6)}`);
+console.log(`TASK 3: sum(23)(4) = ${sum(23)(4)}`);
+console.log(`TASK 3: sum(2)(4)(4)(7)(4) = ${sum(2)(4)(4)(7)(4)}`);
+
+// TASK 5 : Array methods
+
+Array.prototype.myForEach = function(callback) {
+  for(let i = 0; i < this.length; i++) {
+    callback(this[i], i, this);
+  }
+};
+
+Array.prototype.myMap = function(callback) {
+  let newArr = [];
+
+  for(let i = 0; i < this.length; i++) {
+    let mappedItem = callback(this[i], i, this);
+    newArr[i] = mappedItem;
+  }
+
+  return newArr;
+};
+
+Array.prototype.myFind = function(callback) {
+  let foundItem;
+  for(let i = 0; i < this.length; i++) {
+
+    if(callback(this[i], i, this)) {
+      foundItem = this[i];
+      i = this.length;
+    }
+  }
+  return foundItem;
+};
+
+Array.prototype.myFilter = function(callback) {
+  let foundItemsArray = [];
+  for(let i = 0; i < this.length; i++) {
+
+    if(callback(this[i], i, this)) {
+      foundItemsArray[i] = this[i];
+    }
+  }
+  return foundItemsArray;
+};
+
+// let users = [
+//   {id: 1, name: "John"},
+//   {id: 2, name: "Pete"},
+//   {id: 3, name: "Mary"}
+// ];
+
+console.log('TASK 5: array.myForEach(callback)');
+console.log('TASK 5: array.myMap(callback)');
+console.log('TASK 5: array.myFind(callback)');
+console.log('TASK 5: array.myFilter(callback)');
+
+
