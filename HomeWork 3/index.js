918013c05f0d974b2539fc30f02883e538905532
vadimkalayda <vadosgta1@gmail.com>
2020-11-19 22:10:10 +0200

    task1 is ready

--- HomeWork 3/index.js
+++ HomeWork 3/index.js
@@ -1,3 +1,27 @@
+//TASK 1: FOR
+
+const myFor = (indexNumber, condition, body, isPlusStep) => {
+  let i = indexNumber;
+  if(eval(condition)) {
+    eval(body);
+    if(isPlusStep) {
+      i++;
+      return myFor(indexNumber = i++, condition, body, isPlusStep);
+    } else {
+      i--;
+      return myFor(indexNumber = i--, condition, body, isPlusStep);
+    }
+  }
+};
+
+console.log('TASK 1: myFor(indexNumber, condition, body, isPlusStep)');
+console.log('TASK 1: *indexNumber = only numbers(ex: 0)');
+console.log('TASK 1: *condition = only strings(ex: "i < 5") and letter "i" - required');
+console.log('TASK 1: *body = only strings(ex: "console.log(1)")');
+console.log('TASK 1: *isPlusStep = only boolean(true = i++/false = i--');
+console.log('TASK 1: example: myFor(0, i < 5, console.log(new Date()), true)');
+myFor(0, 'i < 5', 'console.log(new Date())', true);
+
 // TASK 2: SUM
 const sum = (a) => {
   let sumNumber = a;
@@ -10,3 +34,8 @@ const sum = (a) => {
   curry.toString = () => sumNumber;
   return curry;
 };
+
+console.log('TASK 2: sum(num1)(num2)...');
+console.log(`TASK 2: sum(2)(4)(6) = ${sum(2)(4)(6)}`);
+console.log(`TASK 2: sum(23)(4) = ${sum(23)(4)}`);
+console.log(`TASK 2: sum(2)(4)(4)(7)(4) = ${sum(2)(4)(4)(7)(4)}`);
