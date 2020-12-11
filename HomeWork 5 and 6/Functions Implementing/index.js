9f80d6f0f39f93a62227340f4f7877770a8942ba
vadimkalayda <vadosgta1@gmail.com>
2020-12-12 01:13:19 +0200

    indexOf function is ready

--- HomeWork 5 and 6/Functions Implementing/index.js
+++ HomeWork 5 and 6/Functions Implementing/index.js
@@ -1,4 +1,18 @@
-//6. Reverse Function
+//4. isSorted Function
+let isSorted = (arr) => {
+
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = i; j < arr.length; j++) {
+      if(arr[i] > arr[j]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
+
+//5. Reverse Function
 let reverse = (myString) => {
   let newString = '';
 
@@ -7,4 +21,17 @@ let reverse = (myString) => {
   }
 
   return newString;
+};
+
+//6. indexOf Function.
+let indexOf = (arr, num) => {
+  let indexOfNumber = -1;
+
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === num) {
+      indexOfNumber = i;
+    }
+  }
+
+  return indexOfNumber;
 };
--- HomeWork 5 and 6/README.md
+++ HomeWork 5 and 6/README.md
@@ -16,5 +16,5 @@ perform(20, function(value) { <br>
   return param; <br>
 }); <br>
 2) Asynchronous-debounce-search --- ✔<br>
-3)
+3) Tagging: https://kikill95.github.io/Tagging/ - vanilla js
 4) Implement these functions: https://gist.githubusercontent.com/kikill95/89440b01a1680dfd969f3ba03c91b8f8/raw/9593f3d67be6f2e8891ecfdcbeab9b0e5b564f21/easy_coding.js
